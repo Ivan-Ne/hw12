d35fea61ff19841cfc62f7aee96f0061f5445c4a
neverov.iv@gmail.com <neverov.iv@gmail.com>
2024-04-14 17:13:25 +0300

    The second commit today

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,17 @@ from selene import Browser, Config
 
 from utils import attach
 
+from dotenv import load_dotenv
+import os
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
@@ -19,7 +30,7 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
 
