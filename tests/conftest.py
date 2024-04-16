1d242c79b5495f59c245af8e8abbaf2a25fdb2f8
neverov.iv@gmail.com <neverov.iv@gmail.com>
2024-04-16 22:31:22 +0300

    The first commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,39 @@
+import os
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
-
 from utils import attach
 
 from dotenv import load_dotenv
-import os
 
-@pytest.fixture(scope='function', autouse=True)
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
+
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     selenoid_login = os.getenv("SELENOID_LOGIN")
     selenoid_pass = os.getenv("SELENOID_PASS")
     selenoid_url = os.getenv("SELENOID_URL")
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "122.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
